5433bdf7eb931f62f2e850278407c55c5f61337a
Poorna Chandu Gade <gadechandu123@gmail.com>
2019-06-01 21:45:24 +0530

    component extraction and reuse

--- js/dynapost.js
+++ js/dynapost.js
@@ -6,44 +6,112 @@ var data = [{
   text: "This came to the eyes of Judges and they can't be stopped about how beautiful this image is. Its beauty lies in its simplicity. It is drawn by a child of 11years who aspires to be an artist.",
   date: 2,
   month: "may"
+}, {
+  img: "batman.jpg",
+  title: "Batman",
+  text: "Batman art",
+  date: 10,
+  month: "may"
+}, {
+  img: "jungle.jpg",
+  title: "Lake View",
+  text: "Animals",
+  date: 1,
+  month: "feb"
+}, {
+  img: "tiger.jpg",
+  title: "Tiger",
+  text: "Tiger from the britannia biscuit",
+  date: 6,
+  month: "apr"
 }];
 
-var html = React.createElement(
-  "article",
-  { "class": "post-5 right-align line" },
-  React.createElement(
-    "div",
-    { "class": "s-12 l-6 post-image" },
-    React.createElement("img", { src: "img/" + data[0].img, alt: "Fashion 1" })
-  ),
-  React.createElement(
-    "div",
-    { "class": "s-12 l-5 post-text" },
-    React.createElement(
-      "h2",
-      null,
-      data[0].title
-    ),
-    React.createElement(
-      "p",
-      null,
-      data[0].text
-    )
-  ),
-  React.createElement(
-    "div",
-    { "class": "s-12 l-1 post-date" },
-    React.createElement(
-      "p",
-      { "class": "date" },
-      data[0].date
-    ),
-    React.createElement(
-      "p",
-      { "class": "month" },
-      data[0].month
-    )
-  )
-);
+function Article(props) {
+  var i = props.i;
+  if ((i + 1) % 2 == 0) {
+    return React.createElement(
+      "article",
+      { "class": "post-" + (i + 1) % 5 + " right-align line" },
+      React.createElement(
+        "div",
+        { "class": "s-12 l-6 post-image" },
+        React.createElement("img", { src: "img/" + props.data.img, alt: "Fashion 1" })
+      ),
+      React.createElement(
+        "div",
+        { "class": "s-12 l-5 post-text" },
+        React.createElement(
+          "h2",
+          null,
+          props.data.title
+        ),
+        React.createElement(
+          "p",
+          null,
+          props.data.text
+        )
+      ),
+      React.createElement(
+        "div",
+        { "class": "s-12 l-1 post-date" },
+        React.createElement(
+          "p",
+          { "class": "date" },
+          props.data.date
+        ),
+        React.createElement(
+          "p",
+          { "class": "month" },
+          props.data.month
+        )
+      )
+    );
+  } else {
+    return React.createElement(
+      "article",
+      { "class": "post-" + (i + 1) % 5 + " line" },
+      React.createElement(
+        "div",
+        { "class": "s-12 l-6 post-image" },
+        React.createElement("img", { src: "img/" + props.data.img, alt: "Fashion 1" })
+      ),
+      React.createElement(
+        "div",
+        { "class": "s-12 l-5 post-text" },
+        React.createElement(
+          "h2",
+          null,
+          props.data.title
+        ),
+        React.createElement(
+          "p",
+          null,
+          props.data.text
+        )
+      ),
+      React.createElement(
+        "div",
+        { "class": "s-12 l-1 post-date" },
+        React.createElement(
+          "p",
+          { "class": "date" },
+          props.data.date
+        ),
+        React.createElement(
+          "p",
+          { "class": "month" },
+          props.data.month
+        )
+      )
+    );
+  }
+}
 
+var html = React.createElement(
+  "div",
+  null,
+  data.map(function (value, index) {
+    return React.createElement(Article, { data: value, i: index, key: index });
+  })
+);
 ReactDOM.render(html, document.getElementById("time-line"));
--- src/dynapost.js
+++ src/dynapost.js
@@ -8,23 +8,72 @@ var data = [
       "This came to the eyes of Judges and they can't be stopped about how beautiful this image is. Its beauty lies in its simplicity. It is drawn by a child of 11years who aspires to be an artist.",
     date: 2,
     month: "may"
+  },
+  {
+    img: "batman.jpg",
+    title: "Batman",
+    text: "Batman art",
+    date: 10,
+    month: "may"
+  },
+  {
+    img: "jungle.jpg",
+    title: "Lake View",
+    text: "Animals",
+    date: 1,
+    month: "feb"
+  },
+  {
+    img: "tiger.jpg",
+    title: "Tiger",
+    text: "Tiger from the britannia biscuit",
+    date: 6,
+    month: "apr"
   }
 ];
 
-const html = (
-  <article class={"post-5 right-align line"}>
-    <div class="s-12 l-6 post-image">
-      <img src={"img/" + data[0].img} alt="Fashion 1" />
-    </div>
-    <div class="s-12 l-5 post-text">
-      <h2>{data[0].title}</h2>
-      <p>{data[0].text}</p>
-    </div>
-    <div class="s-12 l-1 post-date">
-      <p class="date">{data[0].date}</p>
-      <p class="month">{data[0].month}</p>
-    </div>
-  </article>
-);
+function Article(props) {
+  let i = props.i;
+  if ((i + 1) % 2 == 0) {
+    return (
+      <article class={"post-" + ((i + 1) % 5) + " right-align line"}>
+        <div class="s-12 l-6 post-image">
+          <img src={"img/" + props.data.img} alt="Fashion 1" />
+        </div>
+        <div class="s-12 l-5 post-text">
+          <h2>{props.data.title}</h2>
+          <p>{props.data.text}</p>
+        </div>
+        <div class="s-12 l-1 post-date">
+          <p class="date">{props.data.date}</p>
+          <p class="month">{props.data.month}</p>
+        </div>
+      </article>
+    );
+  } else {
+    return (
+      <article class={"post-" + ((i + 1) % 5) + " line"}>
+        <div class="s-12 l-6 post-image">
+          <img src={"img/" + props.data.img} alt="Fashion 1" />
+        </div>
+        <div class="s-12 l-5 post-text">
+          <h2>{props.data.title}</h2>
+          <p>{props.data.text}</p>
+        </div>
+        <div class="s-12 l-1 post-date">
+          <p class="date">{props.data.date}</p>
+          <p class="month">{props.data.month}</p>
+        </div>
+      </article>
+    );
+  }
+}
 
+let html = (
+  <div>
+    {data.map((value, index) => {
+      return <Article data={value} i={index} key={index} />;
+    })}
+  </div>
+);
 ReactDOM.render(html, document.getElementById("time-line"));
